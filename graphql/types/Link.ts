9d87eba802757f925d26545b34d77b0ed79f2b11
Byron Dunkley <ByronDunkley@Shans-MacBook-Pro-2.local>
2022-08-30 13:36:46 +0100

    add nexus set up schema and add appollo to f/e

---
+++ graphql/types/Link.ts
@@ -0,0 +1,39 @@
+import { objectType, extendType  } from "nexus";
+
+import { User } from "./User";
+
+export const Link = objectType({
+    name: 'Link',
+    definition(t) {
+        t.string('id')
+        t.string('title'),
+        t.string('url')
+        t.string('description')
+        t.string('imageUrl')
+        t.string('category')
+        t.list.field('users', {
+            type: User,
+            async resolve(_parent, _args, ctx) {
+                console.log(_parent)
+                return await ctx.prisma.link
+                    .findUnique({
+                        where: {
+                            id: _parent.id
+                        }
+                    }).users()
+            }
+        })
+    },
+})
+
+export const LinksQuery = extendType({
+    type: 'Query',
+    definition(t) {
+        t.nonNull.list.field('links', {
+            type: 'Link',
+            resolve(_parent, _args, ctx) {
+                return ctx.prisma.link.findMany()
+            }
+        })
+    },
+})
