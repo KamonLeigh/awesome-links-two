eafc103cae4c242c222db244225c6dae99f51a8c
Byron Dunkley <ByronDunkley@Shans-MacBook-Pro-2.local>
2022-10-07 00:49:13 +0100

    update apollo

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,8 +1,17 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { relayStylePagination} from '@apollo/client/utilities'
 
 const apolloClient = new ApolloClient({
     uri: 'http://localhost:3001/api/graphql',
-    cache: new InMemoryCache()
+    cache: new InMemoryCache({
+        typePolicies: {
+            Query: {
+                fields: {
+                    links: relayStylePagination()
+                }
+            }
+        }
+    })
 });
 
 export default apolloClient
